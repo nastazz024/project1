8a318ef018f9be499f8d37e7af2ae8c014e25a27
nastazz024 <37698942+nastazz024@users.noreply.github.com>
2018-03-25 21:48:43 +0300

    Create filter.php

---
+++ filter.php
@@ -0,0 +1,128 @@
+<aside class="sidebar">
+					<div class="accordion">
+						<div class="accordion__head open">
+							<h5 class="accordion__title">Тип:</h5>
+							<span class="icon-minus"></span>
+						</div>
+						<div class="accordion__body">
+							<ul class="check-list">
+								<li class="check-list__item">
+									<input type="checkbox" class="styler" id="check-list1">
+									<label for="check-list1" class="check-list__text">Майки</label>
+								</li>
+								<li class="check-list__item">
+									<input type="checkbox" class="styler" id="check-list1">
+									<label for="check-list1" class="check-list__text">Шорты и брюки</label>
+								</li>
+								<li class="check-list__item">
+									<input type="checkbox" class="styler" id="check-list1">
+									<label for="check-list1" class="check-list__text">Куртки и толстовки</label>
+								</li>
+								
+							</ul>
+
+						</div>
+						<div class="accordion__head">
+							<h5 class="accordion__title">Цвет</h5>
+							<span class="icon-minus"></span>
+						</div>
+						<div class="accordion__body" style="display: none;">
+							<ul class="check-list">
+								<li class="check-list__item">
+									<input type="checkbox" class="styler" id="check-list1">
+									<label for="check-list1" class="check-list__text">Черный</label>
+								</li>
+								<li class="check-list__item">
+									<input type="checkbox" class="styler" id="check-list1">
+									<label for="check-list1" class="check-list__text">Белый</label>
+								</li>
+								<li class="check-list__item">
+									<input type="checkbox" class="styler" id="check-list1">
+									<label for="check-list1" class="check-list__text">Красный</label>
+								</li>
+								<li class="check-list__item">
+									<input type="checkbox" class="styler" id="check-list1">
+									<label for="check-list1" class="check-list__text">Голубой</label>
+								</li>
+								<li class="check-list__item">
+									<input type="checkbox" class="styler" id="check-list1">
+									<label for="check-list1" class="check-list__text">Желтый</label>
+								</li>
+								<li class="check-list__item">
+									<input type="checkbox" class="styler" id="check-list1">
+									<label for="check-list1" class="check-list__text">Серый</label>
+								</li>
+								<li class="check-list__item">
+									<input type="checkbox" class="styler" id="check-list1">
+									<label for="check-list1" class="check-list__text">Зеленый</label>
+								</li>
+								<li class="check-list__item">
+									<input type="checkbox" class="styler" id="check-list1">
+									<label for="check-list1" class="check-list__text">Розовый</label>
+								</li>
+								<li class="check-list__item">
+									<input type="checkbox" class="styler" id="check-list1">
+									<label for="check-list1" class="check-list__text">Оранжевый</label>
+								</li>
+								<li class="check-list__item">
+									<input type="checkbox" class="styler" id="check-list1">
+									<label for="check-list1" class="check-list__text">Коралловый</label>
+								</li>
+							</ul>
+						</div>
+						<!--<div class="accordion__head">
+							<h5 class="accordion__title">Size</h5>
+							<span class="icon-minus"></span>
+						</div>
+						<div class="accordion__body" style="display: none;">
+							<ul class="check-list check-list--column">
+								<li class="check-list__item">
+									<input type="checkbox" class="styler" id="check-list1">
+									<label for="check-list1" class="check-list__text">S</label>
+								</li>
+								<li class="check-list__item">
+									<input type="checkbox" class="styler" id="check-list1">
+									<label for="check-list1" class="check-list__text">XL</label>
+								</li>
+								<li class="check-list__item">
+									<input type="checkbox" class="styler" id="check-list1">
+									<label for="check-list1" class="check-list__text">M</label>
+								</li>
+								<li class="check-list__item">
+									<input type="checkbox" class="styler" id="check-list1">
+									<label for="check-list1" class="check-list__text">XXL</label>
+								</li>
+								<li class="check-list__item">
+									<input type="checkbox" class="styler" id="check-list1">
+									<label for="check-list1" class="check-list__text">L</label>
+								</li>
+								<li class="check-list__item">
+									<input type="checkbox" class="styler" id="check-list1">
+									<label for="check-list1" class="check-list__text">XXXL</label>
+								</li>
+
+							</ul>
+						</div>-->
+						<div class="accordion__head">
+							<h5 class="accordion__title">Цена</h5>
+							<span class="icon-minus"></span>
+						</div>
+						<div class="accordion__body" style="display: none;">
+
+							<div id="slider-range"></div>
+							<input type="text" id="amount" readonly style="border:0; color:#f6931f; font-weight:bold;">
+							<input type="text" id="amount1" readonly style="border:0; color:#f6931f; font-weight:bold;">
+
+						</div>
+						<!--<div class="accordion__head">
+							<h5 class="accordion__title">Brand</h5>
+							<span class="icon-minus"></span>
+						</div>
+						<div class="accordion__body" style="display: none;">22</div>
+						<div class="accordion__head">
+							<h5 class="accordion__title">Brand</h5>
+							<span class="icon-minus"></span>
+						</div>
+						<div class="accordion__body" style="display: none;">22</div>-->
+					</div>
+				</aside>
