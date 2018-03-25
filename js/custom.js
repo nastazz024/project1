7ec74a9cedccb471064e5aa08f2b77a0f6253312
nastazz024 <37698942+nastazz024@users.noreply.github.com>
2018-03-25 21:51:44 +0300

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,39 @@
+$(function(){
+
+	$('.tabs a').click(function(){
+		$(this).parents('.tab-wrap').find('.tab-cont').addClass('hide');
+		$(this).parent().siblings().removeClass('active');
+		var id = $(this).attr('href');
+		$(id).removeClass('hide');
+		$(this).parent().addClass('active');
+		return false
+	});
+
+	$('.accordion__head').on('click', function(){
+		var el = $(this);
+		el.next('.accordion__body').slideToggle();
+		el.toggleClass('open');
+		return false;
+	});
+
+	$('.styler').styler();
+
+	$( "#slider-range" ).slider({
+		range: true,
+		min: 0,
+		max: 500,
+		values: [ 75, 300 ],
+		slide: function( event, ui ) {
+			$( "#amount" ).val( "Br " + ui.values[ 0 ] );
+			$( "#amount1" ).val( "Br " + ui.values[ 1 ] );
+		}
+    });
+    $( "#amount" ).val( "Br " + $( "#slider-range" ).slider( "values", 0 ) );
+    $( "#amount1" ).val( "Br " + $( "#slider-range" ).slider( "values", 1 ) );
+	
+
+});
+
+   
+
+
