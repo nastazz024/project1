18fc6abf73f0365e4c8f370fd834edf85c7c8b4e
nastazz024 <37698942+nastazz024@users.noreply.github.com>
2018-03-25 21:49:04 +0300

    Create header.php

---
+++ header.php
@@ -0,0 +1,221 @@
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="format-detection" content="telephone=no">
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no, maximum-scale=1">
+	<title>FZ FORZA</title>
+	<link href="https://fonts.googleapis.com/css?family=Oswald:400,500,600,700" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=PT+Serif:400,400i,700,700i" rel="stylesheet">
+	<link rel="stylesheet" href="css/icomoon.css">
+	<link rel="stylesheet" href="css/jquery.formstyler.css">
+	<link rel="stylesheet" href="css/jquery.formstyler.theme.css">
+	<link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css" />				
+</head>
+<body>
+
+<div class="main-wrapper">
+
+	<header class="header">
+		
+		<div class="logo">
+			<a href="index.php"><img src="img/logo.png" alt=""></a>
+		</div>
+
+		<ul class="main-menu">
+			<li class="main-menu__item dropdown">
+				<a href="#" class="main-menu__link">Одежда <span class="icon-caret"></span></a>
+				<div class="dropdown-content">
+					<ul class="dropdown-menu">
+						<li class="dropdown-menu__item">
+                       
+							<a href="catalog.php?men" name="men" class="dropdown-menu__link">Мужская</a>
+                            
+                            
+                            
+                       
+						</li>
+						<li class="dropdown-menu__item">
+							<a href="#" class="dropdown-menu__link">Женская</a>
+						</li>
+						<li class="dropdown-menu__item">
+							<a href="#" class="dropdown-menu__link">Детская</a>
+						</li>
+						
+					</ul>
+
+				</div>
+			</li>
+			<li class="main-menu__item dropdown">
+				<a href="#" class="main-menu__link">Ракетки <span class="icon-caret"></span></a>
+				<div class="dropdown-content">
+					<ul class="dropdown-menu">
+						<li class="dropdown-menu__item">
+							<a href="#" class="dropdown-menu__link">Ракетки</a>
+						</li>
+						<li class="dropdown-menu__item">
+							<a href="#" class="dropdown-menu__link">Аксессуары для ракетки</a>
+						</li>
+						
+					</ul>
+
+
+				</div>
+			</li>
+			<li class="main-menu__item dropdown">
+				<a href="#" class="main-menu__link">Обувь <span class="icon-caret"></span></a>
+				<div class="dropdown-content">
+					<ul class="dropdown-menu">
+						<li class="dropdown-menu__item">
+							<a href="#" class="dropdown-menu__link">Мужская</a>
+						</li>
+						<li class="dropdown-menu__item">
+							<a href="#" class="dropdown-menu__link">Женская</a>
+						</li>
+						<li class="dropdown-menu__item">
+							<a href="#" class="dropdown-menu__link">Детская</a>
+						</li>
+						
+					</ul>
+
+				</div>
+			</li>
+			<li class="main-menu__item dropdown">
+				<a href="#" class="main-menu__link">Сумки и аксессуары <span class="icon-caret"></span></a>
+				<div class="dropdown-content">
+					<ul class="dropdown-menu">
+						<li class="dropdown-menu__item">
+							<a href="#" class="dropdown-menu__link">Сумки</a>
+						</li>
+						<li class="dropdown-menu__item">
+							<a href="#" class="dropdown-menu__link">Аксессуары</a>
+						</li>
+						
+					</ul>
+
+				</div>
+			</li>
+			<li class="main-menu__item">
+				<a href="#" class="main-menu__link">Контакты</a>
+			</li>
+			
+		</ul>
+
+		<div class="search">
+			<form action="#">
+				<input type="text" class="form-control search__input" placeholder="Поиск" />
+				<input type="submit" value="&#xe90b;" class="search__button"/>
+			</form>
+		</div>
+
+		<div class="location">
+			<span class="icon-pin"></span>
+			<a href="#" class="location__link">Минск / Br</a>
+		</div>
+		
+		<ul class="profile-menu">
+			<li class="profile-menu__item dropdown dropdown--right dropdown--white">
+				<a href="#" class="profile-menu__link">
+					<span class="basket">
+						<span class="icon-bag"></span>
+						<span class="badge">3</span>
+					</span>
+					<span class="b-price">165 Br</span>
+				</a>
+				<div class="dropdown-content">
+					<div class="cart">
+						<ul class="cart-list">
+							<li class="cart-list__item">
+								<div class="cart-list__img">
+									<a href="#"><img src="img/cart-list-img.png" alt=""></a>
+								</div>
+								<div class="cart-list__info">
+									<h6 class="product__title"><a href="#">New Balance Men's Street Backpack</a></h6>
+									<div class="cart-list__details">
+										<span class="price">485 Br</span>
+										<span class="product__details">
+											<span>S</span> • 
+											<span>Черный</span> • 
+											<span>1 pcs</span>  
+										</span>
+									</div>
+								</div>
+								<div class="cart-list__delete">
+									<a href="#" class="js-remove-cart">
+										<span class="icon-close"><span class="path1"></span><span class="path2"></span></span>
+									</a>
+								</div>
+							</li>
+							<li class="cart-list__item">
+								<div class="cart-list__img">
+									<a href="#"><img src="img/cart-list-img1.png" alt=""></a>
+								</div>
+								<div class="cart-list__info">
+									<h6 class="product__title"><a href="#">Lyle & Scott Men's Short Sleeve Camo Pocket</a></h6>
+									<div class="cart-list__details">
+										<span class="price price--danger">85 Br</span>
+										<span class="product__details">  
+											<span>XXL</span> • 
+											<span>Белый</span> • 
+											<span>3 pcs</span>  
+										</span>
+									</div>
+								</div>
+								<div class="cart-list__delete">
+									<a href="#" class="js-remove-cart">
+										<span class="icon-close"><span class="path1"></span><span class="path2"></span></span>
+									</a>
+								</div>
+							</li>
+							<li class="cart-list__item">
+								<div class="cart-list__img">
+									<a href="#"><img src="img/cart-list-img2.png" alt=""></a>
+								</div>
+								<div class="cart-list__info">
+									<h6 class="product__title"><a href="#">Lyle & Scott Men's Short Sleeve Camo Pocket</a></h6>
+									<div class="cart-list__details">
+										<span class="price">128 Br</span>
+										<span class="product__details">
+											<span>M</span> • 
+											<span>Оранжевый</span> • 
+											<span>89 pcs</span>  
+										</span>
+									</div>
+								</div>
+								<div class="cart-list__delete">
+									<a href="#" class="js-remove-cart">
+										<span class="icon-close"><span class="path1"></span><span class="path2"></span></span>
+									</a>
+								</div>
+							</li>
+						</ul>
+						<div class="cart-footer">
+							<span class="total">Total</span>
+							<span class="price">34 Br</span>
+							<a href="#" class="btn">Check Out</a>
+						</div>
+					</div>
+				</div>
+			</li>
+			<li class="profile-menu__item dropdown dropdown--right dropdown--white">
+				<a href="#" class="profile-menu__link"><span class="icon-user"></span></a>
+				<div class="dropdown-content">
+					<ul class="user-menu">
+						
+						<li class="user-menu__item">
+							<a href="#" class="user-menu__link"><span class="icon-gear"></span>Авторизация</a>
+						</li>
+						<li class="user-menu__item">
+							<a href="#" class="user-menu__link"><span class="icon-power"></span>Регистрация</a>
+						</li>
+						<li class="user-menu__item">
+							<a href="#" class="user-menu__link"><span class="icon-user"></span>Выход</a>
+						</li>
+					</ul>
+					
+				</div>
+			</li>
+		</ul>
+	</header>
+</body>
+</html>
