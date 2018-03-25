af838af8b1e6b5fbd0f49b694177e3ffa571bf44
nastazz024 <37698942+nastazz024@users.noreply.github.com>
2018-03-25 21:47:21 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<!--<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="format-detection" content="telephone=no">
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no, maximum-scale=1">
+	<title>FZ FORZA</title>
+	<link href="https://fonts.googleapis.com/css?family=Oswald:400,500,600,700" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=PT+Serif:400,400i,700,700i" rel="stylesheet">
+	<link rel="stylesheet" href="css/icomoon.css">
+	<link rel="stylesheet" href="css/jquery.formstyler.css">
+	<link rel="stylesheet" href="css/jquery.formstyler.theme.css">
+	<link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css" />	-->	
+		
+</head>
+<body>
+
+	<?php
+		include "header.php";
+		
+	?>
+		
+	
+	<footer>
+		
+	</footer>
+	
+
+
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<script src="js/jquery.formstyler.min.js"></script>
+<script src="js/custom.js"></script>
+</body>
+</html>
